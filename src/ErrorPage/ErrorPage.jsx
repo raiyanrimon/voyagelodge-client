f1bb157fd3eccd2b4680fdd6ab728ff293d5bdf4
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2023-11-05 02:45:36 +0600

    404 page added

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Layout/Navbar";
+import img404 from "../assets/Add a heading.webp";
 const ErrorPage = () => {
-  return <div></div>;
+  return (
+    <div className="text-center max-w-7xl mx-auto">
+      <Navbar></Navbar>
+      <img src={img404} alt="" />
+      <Link to="/">
+        <button className="btn btn-neutral">Go Back Home</button>
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Layout/Layout";
+import ErrorPage from "./ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [{}],
   },
 ]);
 
