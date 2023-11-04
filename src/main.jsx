b512aa85c5b390b6314eb3e89b8a8aa4ca8e84e5
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2023-11-05 01:43:59 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout/Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Layout></Layout>,
   },
 ]);
 
